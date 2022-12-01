11b8f41f62adb3edb97ab26af0cb2932c2f4d026
Rio Edwards <rioredwards@gmail.com>
2022-12-01 11:44:34 -0800

    Setup Repo

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,11 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS todos CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  email VARCHAR,
-  password_hash VARCHAR NOT NULL,
-  first_name VARCHAR NOT NULL,
-  last_name VARCHAR NOT NULL
+  email VARCHAR NOT NULL UNIQUE,
+  password_hash VARCHAR NOT NULL
 );
+
+CREATE TABLE todos (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT ,
+  description VARCHAR NOT NULL,
+  complete BOOLEAN NOT NULL DEFAULT(false),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+)
